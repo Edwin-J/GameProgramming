253202031295de12517259d8eea3945c38544adb
Edwin-J <gkdhaosl@naver.com>
2017-08-31 15:07:47 +0900

    0831

---
+++ 0831/Assets/CubeMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMove : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+            transform.position = transform.position + transform.forward;
+        else if (Input.GetKey(KeyCode.S))
+            transform.position = transform.position - transform.forward;
+        else if (Input.GetKey(KeyCode.A))
+            transform.position = transform.position - transform.right;
+        else if (Input.GetKey(KeyCode.D))
+            transform.position = transform.position + transform.right;
+	}
+}
